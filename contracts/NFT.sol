6b3619addc785e657da3041034198a441116205d
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-22 12:12:10 +0530

    Contracts

---
+++ contracts/NFT.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    string public collectionName;
+    string public collectionSymbol;
+
+    constructor(address marketplaceAddress) ERC721("Thinkrs NFT", "THINK") {
+        contractAddress = marketplaceAddress;
+        collectionName = name();
+        collectionSymbol = symbol();
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+async function main() {
+	const NFTMarket = await hre.ethers.getContractFactory('NFTMarket')
+	const nftMarket = await NFTMarket.deploy()
+	await nftMarket.deployed()
+	console.log('nftMarket contract deployed to:', nftMarket.address)
+
+	const NFT = await hre.ethers.getContractFactory('NFT')
+	const nft = await NFT.deploy(nftMarket.address)
+	await nft.deployed()
+	console.log('nft contract deployed to:', nft.address)
+}
+
+const runMain = async () => {
+	try {
+		await main()
+		process.exit(0)
+	} catch (error) {
+		console.error(error)
+		process.exit(1)
+	}
+}
+
+runMain()
