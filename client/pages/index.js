d0e631c5272f1807104c1a60f84611c2fd1cb6df
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-30 02:55:55 +0530

    head

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<div className='flex flex-col mt-40 min-h-screen text-gray-200'>
 			<Head>
-				<title>Create Next App</title>
+				<title>Thinkrs</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Hero />
