6da87bdafb18cf45bbe94d028da15c808a38f407
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-30 20:01:55 +0530

    media queries initial

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<div className='bg-black min-h-screen'>
+		<div className='bg-black min-h-screen min-w-fit'>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
