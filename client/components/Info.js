b751a9b92524e9b120d17377fd3a58d6c20c539b
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-31 12:02:35 +0530

    Info component media query

--- client/components/Info.js
+++ client/components/Info.js
@@ -1,7 +1,7 @@
 const Info = () => {
 	return (
-		<div className='flex justify-between items-center px-40 gap-x-20 mt-36 py-20 bg-green-400 text-gray-900'>
-			<div className='flex flex-col gap-y-4 items-center'>
+		<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 mt-20 p-20 bg-green-400 text-gray-900'>
+			<div className='flex flex-col gap-y-4 items-center px-80 sm:px-0'>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					width='40'
@@ -13,7 +13,7 @@ const Info = () => {
 					<path d='M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z' />
 				</svg>
 				<div className='font-bold text-3xl'>Set up your wallet</div>
-				<div>
+				<div className='text-center'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ut quas
 					corrupti, maxime animi quis et quae laboriosam. Debitis amet
 					dignissimos, doloribus qui ratione quam explicabo nostrum? Minima,
@@ -21,7 +21,7 @@ const Info = () => {
 					maxime quaerat dicta at nesciunt?
 				</div>
 			</div>
-			<div className='flex flex-col gap-y-4 items-center'>
+			<div className='flex flex-col gap-y-4 items-center px-80 sm:px-0'>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					width='40'
@@ -33,7 +33,7 @@ const Info = () => {
 					<path d='M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z' />
 				</svg>
 				<div className='font-bold text-3xl'>Create your collection</div>
-				<div>
+				<div className='text-center'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ut quas
 					corrupti, maxime animi quis et quae laboriosam. Debitis amet
 					dignissimos, doloribus qui ratione quam explicabo nostrum? Minima,
@@ -41,7 +41,7 @@ const Info = () => {
 					maxime quaerat dicta at nesciunt?
 				</div>
 			</div>
-			<div className='flex flex-col gap-y-4 items-center'>
+			<div className='flex flex-col gap-y-4 items-center px-80 sm:px-0'>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					width='40'
@@ -54,7 +54,7 @@ const Info = () => {
 					<path d='M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z' />
 				</svg>
 				<div className='font-bold text-3xl'>Add your NFTs</div>
-				<div>
+				<div className='text-center'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ut quas
 					corrupti, maxime animi quis et quae laboriosam. Debitis amet
 					dignissimos, doloribus qui ratione quam explicabo nostrum? Minima,
@@ -62,7 +62,7 @@ const Info = () => {
 					maxime quaerat dicta at nesciunt?
 				</div>
 			</div>
-			<div className='flex flex-col gap-y-4 items-center'>
+			<div className='flex flex-col gap-y-4 items-center px-80 sm:px-0'>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					width='40'
@@ -75,7 +75,7 @@ const Info = () => {
 					<path d='M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z' />
 				</svg>
 				<div className='font-bold text-3xl'>List them for sale</div>
-				<div>
+				<div className='text-center'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ut quas
 					corrupti, maxime animi quis et quae laboriosam. Debitis amet
 					dignissimos, doloribus qui ratione quam explicabo nostrum? Minima,
