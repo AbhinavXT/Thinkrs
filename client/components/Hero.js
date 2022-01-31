65a41c6714a2956b399cefa762c9c309ee094ec4
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-31 16:00:35 +0530

    index.js media queries

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -2,37 +2,39 @@ import Link from 'next/link'
 
 const Hero = () => {
 	return (
-		<div className='flex flex-col items-center gap-y-32 text-center px-40 gap-x-20 md:flex-row md:text-left'>
-			<div className='flex flex-col gap-y-8 w-1/2'>
-				<div className='font-extrabold text-5xl'>
+		<div className='flex flex-col items-center gap-y-32 text-center px-40 md:gap-x-20 md:flex-row md:text-left'>
+			<div className='flex flex-col gap-y-8 lg:w-1/2'>
+				<div className='font-extrabold text-[60px] md:text-5xl'>
 					Trade NFTs of News Stories, Earn and Rewards
 				</div>
-				<div className='font-bold'>
+				<div className='text-3xl px-12 md:text-lg md:px-0'>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
-					totam vel exercitationem odio doloribus vero labore magnam suscipit.
-					Delectus totam neque eveniet pariatur hic alias odio ullam doloremque
-					quod quam? Numquam pariatur quam animi reprehenderit, alias culpa
-					quisquam fugiat libero laudantium itaque dolor aliquid perspiciatis
-					repudiandae odio sint quos iusto ad minima vero? Modi reiciendis
-					labore expedita sapiente quisquam dolorem! Repudiandae, incidunt
-					magnam! Suscipit, debitis tenetur? Possimus maiores nam minus qui
-					excepturi fugit quasi distinctio ea?
+					totam vel exercitationem odio doloribus vero labore magnam suscipit.{' '}
+					<span className='hidden md:inline'>
+						Delectus totam neque eveniet pariatur hic alias odio ullam
+						doloremque quod quam? Numquam pariatur quam animi reprehenderit,
+						alias culpa quisquam fugiat libero laudantium itaque dolor aliquid
+						perspiciatis repudiandae odio sint quos iusto ad minima vero? Modi
+						reiciendis labore expedita sapiente quisquam dolorem! Repudiandae,
+						incidunt magnam! Suscipit, debitis tenetur? Possimus maiores nam
+						minus qui excepturi fugit quasi distinctio ea?
+					</span>
 				</div>
 				<div className='flex gap-x-8 justify-center md:justify-start'>
 					<Link href='/explore'>
-						<button className='p-4 text-black font-bold rounded-lg bg-green-400 hover:scale-[1.03] transtion duration-500'>
+						<button className='p-4 text-black font-bold rounded-lg bg-green-400 hover:scale-[1.03] transtion duration-500 text-3xl md:text-lg'>
 							Explore NFTs
 						</button>
 					</Link>
 					<Link href='/create'>
-						<button className='p-4 font-bold rounded-lg bg-gray-900 hover:scale-[1.03] transtion duration-500'>
+						<button className='p-4 font-bold rounded-lg bg-gray-900 hover:scale-[1.03] transtion duration-500 text-3xl md:text-lg'>
 							List an NFT
 						</button>
 					</Link>
 				</div>
 			</div>
 			<div className='flex flex-col gap-y-4 mx-40'>
-				<div className='bg-gray-900 h-[415px] w-[380px] rounded-2xl shadow-green-400 shadow-lg hover:scale-[1.02] transtion duration-500'>
+				<div className='bg-gray-900 h-[525px] md:h-[415px] md:w-[380px] rounded-2xl shadow-green-400 shadow-lg hover:scale-[1.02] transtion duration-500'>
 					<div className='p-4 flex justify-between'>
 						<div className='flex items-center justify-center font-bold px-12 h-8 w-20 bg-gray-700 rounded-lg'>
 							Trending
