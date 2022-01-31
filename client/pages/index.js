b8d85bc3d3dcb0a0ab60c719bca9d367a3b6d2cf
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-31 11:22:48 +0530

    Width and height change

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,7 +5,7 @@ import Info from '../components/Info'
 
 export default function Home() {
 	return (
-		<div className='flex flex-col mt-40 text-gray-200'>
+		<div className='flex flex-col mt-40 min-h-screen text-gray-200'>
 			<Head>
 				<title>Thinkrs</title>
 				<link rel='icon' href='/favicon.ico' />
